b96f2ef2b9509f72c8ea1da290df97acb9e196d1
Frank <frank.boeddeker@gmx.net>
2016-06-29 07:35:26 +0200

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,10 @@
+import { combineReducers } from 'redux'
+import messages from './messageReducer.js'
+import members from './memberReducer.js'
 
+const chatApp = combineReducers({
+    messages,
+    members
+});
+
+export default chatApp;
